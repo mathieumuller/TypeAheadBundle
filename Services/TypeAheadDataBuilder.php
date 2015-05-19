65c7cd07b1b8fc573a1cd3593759b78bb68e19fb
mathieumuller <mathieu.1984@live.fr>
2015-05-19 22:15:37 +0200

    maj

---
+++ Services/TypeAheadDataBuilder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MatM\Bundle\TypeAheadBundle\Services;
+
+class TypeAheadDataBuilder
+{
+    public function makeTypeAheadDataset($results, $displayMethod, $searchMethod)
+    {
+        $list = array();
+
+        foreach ($results as $result) {
+            $reflectionClass = new \ReflectionClass(get_class($result));
+
+            if ($reflectionClass->hasMethod($displayMethod) && $reflectionClass->hasMethod($searchMethod)) {
+                $list[] = array(
+                    "displayed_value" => $result->{$displayMethod}(),
+                    "selected_value"  => $result->getId(),
+                    "search_value"    => $result->{$searchMethod}()
+                );
+            }
+        }
+
+        return $list;
+    }
+}
