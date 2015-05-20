482e9e41035ca9561a9928b1220fed3867837e9c
mathieumuller <mathieu.1984@live.fr>
2015-05-20 10:07:35 +0200

    Update TypeAheadDataBuilder.php

--- Services/TypeAheadDataBuilder.php
+++ Services/TypeAheadDataBuilder.php
@@ -19,8 +19,6 @@ class TypeAheadDataBuilder
                     "selected_value"  => $result->getId(),
                     "search_value"    => $result->{$searchMethod}()
                 );
-            } else {
-                throw new UndefinedMethodException();
             }
         }
 
